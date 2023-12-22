702c36de74af528f23d83a6b5f56e1ee622188ec
hako-975 <andrifirmansaputra1@gmail.com>
2023-12-22 17:52:43 +0700

    fixed web browser using stack

--- JepangCita/Assets/Scripts/Panels/Desk/SearchInputBrowser.cs
+++ JepangCita/Assets/Scripts/Panels/Desk/SearchInputBrowser.cs
@@ -1,11 +1,13 @@
 using UnityEngine;
 using TMPro;
 using System.Collections;
+using System.Collections.Generic;
 using System.Text.RegularExpressions;
 using UnityEngine.UI;
 
 public class SearchInputBrowser : MonoBehaviour
 {
+    [Header("Inputs")]
     [SerializeField]
     private TMP_InputField urlInput;
 
@@ -26,26 +28,45 @@ public class SearchInputBrowser : MonoBehaviour
     private GameObject jepangCitaLoginPanel;
 
     [SerializeField]
-    private GameObject jepangCitaDashboardPanel;
-
-    [SerializeField]
     private GameObject notFoundPanel;
 
+    [Header("Buttons")]
     [SerializeField]
     private Button goBackwardButton;
 
     [SerializeField]
     private Button goForwardButton;
 
+    [Header("Navbars")]
+    [SerializeField]
+    private Button logoButton;
+
+    [SerializeField]
+    private Button registerButton;
+
+    [SerializeField]
+    private Button loginButton;
+
     private GameObject currentPanel;
     private GameObject nextPanel;
 
+    private readonly Stack<GameObject> panelHistory = new Stack<GameObject>();
+    private readonly Stack<GameObject> panelFuture = new Stack<GameObject>();
+
     void Start()
     {
+        goBackwardButton.interactable = false;
+        goForwardButton.interactable = false;
+
         currentPanel = mainPanel;
+        panelHistory.Push(currentPanel);
 
         goBackwardButton.onClick.AddListener(Backward);
-        goForwardButton.onClick.AddListener(UpdatePanel);
+        goForwardButton.onClick.AddListener(Forward);
+
+        logoButton.onClick.AddListener(delegate { OnInputEndEdit("https://jepangcita.com"); });
+        registerButton.onClick.AddListener(delegate { OnInputEndEdit("https://jepangcita.com/register"); });
+        loginButton.onClick.AddListener(delegate { OnInputEndEdit("https://jepangcita.com/login"); });
 
         urlInput.onEndEdit.AddListener(OnInputEndEdit);
         searchInput.onEndEdit.AddListener(OnInputEndEdit);
@@ -53,35 +74,61 @@ public class SearchInputBrowser : MonoBehaviour
 
     private void OnInputEndEdit(string input)
     {
-        if (Input.GetKey(KeyCode.Return) || Input.GetKey(KeyCode.KeypadEnter))
+        GameObject panelToShow;
+
+        if (IsJepangCitaInput(input))
         {
-            GameObject panelToShow;
-
-            if (IsJepangCitaInput(input))
-            {
-                urlInput.text = "https://jepangcita.com/";
-                panelToShow = jepangCitaPanel;
-            }
-            else if (IsTemukanInput(input))
-            {
-                urlInput.text = "https://temukan.com/";
-                panelToShow = mainPanel;
-            }
-            else
-            {
-                panelToShow = notFoundPanel;
-            }
-
-            // logic
-            nextPanel = panelToShow;
-            UpdatePanel();
+            urlInput.text = "https://jepangcita.com/";
+            panelToShow = jepangCitaPanel;
         }
+        else if (IsJepangCitaRegisterInput(input))
+        {
+            urlInput.text = "https://jepangcita.com/register";
+            panelToShow = jepangCitaRegisterPanel;
+        }
+        else if (IsJepangCitaLoginInput(input))
+        {
+            urlInput.text = "https://jepangcita.com/login";
+            panelToShow = jepangCitaLoginPanel;
+        }
+        else if (IsTemukanInput(input))
+        {
+            urlInput.text = "https://temukan.com/";
+            panelToShow = mainPanel;
+        }
+        else
+        {
+            panelToShow = notFoundPanel;
+        }
+
+        nextPanel = panelToShow;
+        UpdatePanel();
     }
 
     private bool IsJepangCitaInput(string input)
     {
         string lowercaseInput = Regex.Replace(input, "[^a-zA-Z0-9]", "").Trim().ToLower();
+        if (lowercaseInput.Contains("httpsjepangcitacomlogin") || lowercaseInput.Contains("jepangcitacomlogin"))
+        {
+            return false;
+        }
+        else if (lowercaseInput.Contains("httpsjepangcitacomregister") || lowercaseInput.Contains("jepangcitacomregister"))
+        {
+            return false;
+        }
         return lowercaseInput.Contains("jepangcita") || lowercaseInput.Contains("jepang cita") || lowercaseInput.Contains("jepangcitacom") || lowercaseInput.Contains("httpsjepangcitacom");
+    }
+
+    private bool IsJepangCitaRegisterInput(string input)
+    {
+        string lowercaseInput = Regex.Replace(input, "[^a-zA-Z0-9]", "").Trim().ToLower();
+        return lowercaseInput.Contains("jepangcitaregister") || lowercaseInput.Contains("jepang cita register") || lowercaseInput.Contains("jepangcitacomregister") || lowercaseInput.Contains("httpsjepangcitacomregister");
+    }
+
+    private bool IsJepangCitaLoginInput(string input)
+    {
+        string lowercaseInput = Regex.Replace(input, "[^a-zA-Z0-9]", "").Trim().ToLower();
+        return lowercaseInput.Contains("jepangcitalogin") || lowercaseInput.Contains("jepang cita login") || lowercaseInput.Contains("jepangcitacomlogin") || lowercaseInput.Contains("httpsjepangcitacomlogin");
     }
 
     private bool IsTemukanInput(string input)
@@ -94,7 +141,6 @@ public class SearchInputBrowser : MonoBehaviour
     {
         panel.SetActive(true);
         panel.GetComponent<Animator>().SetTrigger("Show");
-
         yield return null;
     }
 
@@ -106,31 +152,85 @@ public class SearchInputBrowser : MonoBehaviour
 
     private void UpdatePanel()
     {
-        if (nextPanel == jepangCitaPanel)
+        goBackwardButton.interactable = panelHistory.Count > 0;
+
+        if (nextPanel != null)
         {
-            urlInput.text = "https://jepangcita.com/";
+            panelHistory.Push(currentPanel);
+
+            if (currentPanel != null)
+            {
+                StartCoroutine(AnimationCloseWeb(currentPanel));
+            }
+
+            currentPanel = nextPanel;
+            StartCoroutine(AnimationOpenWeb(nextPanel));
+            goForwardButton.interactable = false;
+            panelFuture.Clear(); 
         }
-        else if (nextPanel == mainPanel)
+        else
         {
-            urlInput.text = "https://temukan.com/";
+            goForwardButton.interactable = panelFuture.Count > 0;
         }
 
-        StartCoroutine(AnimationCloseWeb(currentPanel));
-        StartCoroutine(AnimationOpenWeb(nextPanel));
+        UpdateUrlInputText();
     }
 
+
     private void Backward()
+    {
+        if (panelHistory.Count > 1)
+        {
+            panelFuture.Push(currentPanel);
+
+            nextPanel = panelHistory.Pop();
+            StartCoroutine(AnimationCloseWeb(currentPanel));
+            StartCoroutine(AnimationOpenWeb(nextPanel));
+
+            currentPanel = nextPanel;
+            goForwardButton.interactable = true; 
+        }
+
+        goBackwardButton.interactable = panelHistory.Count > 1 || currentPanel != mainPanel;
+        UpdateUrlInputText();
+    }
+
+
+    private void Forward()
+    {
+        if (panelFuture.Count > 0)
+        {
+            panelHistory.Push(currentPanel);
+
+            nextPanel = panelFuture.Pop();
+            StartCoroutine(AnimationCloseWeb(currentPanel));
+            StartCoroutine(AnimationOpenWeb(nextPanel));
+
+            currentPanel = nextPanel;
+            goBackwardButton.interactable = true;
+        }
+
+        goForwardButton.interactable = panelFuture.Count > 0;
+        UpdateUrlInputText();
+    }
+
+    private void UpdateUrlInputText()
     {
         if (currentPanel == jepangCitaPanel)
         {
             urlInput.text = "https://jepangcita.com/";
         }
+        else if (currentPanel == jepangCitaLoginPanel)
+        {
+            urlInput.text = "https://jepangcita.com/login";
+        }
+        else if (currentPanel == jepangCitaRegisterPanel)
+        {
+            urlInput.text = "https://jepangcita.com/register";
+        }
         else if (currentPanel == mainPanel)
         {
             urlInput.text = "https://temukan.com/";
         }
-
-        StartCoroutine(AnimationCloseWeb(nextPanel));
-        StartCoroutine(AnimationOpenWeb(currentPanel));
     }
 }
