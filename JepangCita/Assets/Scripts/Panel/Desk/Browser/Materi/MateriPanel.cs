bfc369e779a89944c19bf66b592867e12eafd9f3
hako-975 <andrifirmansaputra1@gmail.com>
2024-02-09 15:25:28 +0700

    all materi

--- JepangCita/Assets/Scripts/Panel/Desk/Browser/Materi/MateriPanel.cs
+++ JepangCita/Assets/Scripts/Panel/Desk/Browser/Materi/MateriPanel.cs
@@ -13,6 +13,27 @@ public class MateriPanel : MonoBehaviour
     private GameObject contentKatakana;
     [SerializeField]
     private GameObject contentAngka;
+    [SerializeField]
+    private GameObject contentWaktu;
+    [Header("Panels Kata")]
+    [SerializeField]
+    private GameObject contentKataGanti;
+    [SerializeField]
+    private GameObject contentKataBenda;
+    [SerializeField]
+    private GameObject contentKataKerja;
+    [SerializeField]
+    private GameObject contentKataSifat;
+    [SerializeField]
+    private GameObject contentKataKeterangan;
+    [SerializeField]
+    private GameObject contentKataTanya;
+    [SerializeField]
+    private GameObject contentKataHubung;
+    [SerializeField]
+    private GameObject contentKataSeru;
+    [SerializeField]
+    private GameObject contentPerkenalanDiri;
 
     [Header("Buttons")]
     [SerializeField]
@@ -20,11 +41,24 @@ public class MateriPanel : MonoBehaviour
     [SerializeField]
     private Button btnAngka;
     [SerializeField]
+    private Button btnWaktu;
+    [Header("Buttons Kata")]
+    [SerializeField]
+    private Button btnKataGanti;
+    [SerializeField]
     private Button btnKataBenda;
     [SerializeField]
     private Button btnKataKerja;
     [SerializeField]
     private Button btnKataSifat;
+    [SerializeField]
+    private Button btnKataKeterangan;
+    [SerializeField]
+    private Button btnKataTanya;
+    [SerializeField]
+    private Button btnKataHubung;
+    [SerializeField]
+    private Button btnKataSeru;
     [SerializeField]
     private Button btnPerkenalanDiri;
 
@@ -34,6 +68,16 @@ public class MateriPanel : MonoBehaviour
         BackToMateriPanel();
         btnHuruf.onClick.AddListener(delegate { OpenPanel(contentHuruf); });
         btnAngka.onClick.AddListener(delegate { OpenPanel(contentAngka); });
+        btnWaktu.onClick.AddListener(delegate { OpenPanel(contentWaktu); });
+        btnKataGanti.onClick.AddListener(delegate { OpenPanel(contentKataGanti); });
+        btnKataBenda.onClick.AddListener(delegate { OpenPanel(contentKataBenda); });
+        btnKataKerja.onClick.AddListener(delegate { OpenPanel(contentKataKerja); });
+        btnKataSifat.onClick.AddListener(delegate { OpenPanel(contentKataSifat); });
+        btnKataKeterangan.onClick.AddListener(delegate { OpenPanel(contentKataKeterangan); });
+        btnKataTanya.onClick.AddListener(delegate { OpenPanel(contentKataTanya); });
+        btnKataHubung.onClick.AddListener(delegate { OpenPanel(contentKataHubung); });
+        btnKataSeru.onClick.AddListener(delegate { OpenPanel(contentKataSeru); });
+        btnPerkenalanDiri.onClick.AddListener(delegate { OpenPanel(contentPerkenalanDiri); });
     }
 
     private void OpenPanel(GameObject panel)
@@ -41,6 +85,7 @@ public class MateriPanel : MonoBehaviour
         BackToMateriPanel();
         materiPanel.SetActive(false);
         panel.SetActive(true);
+        LayoutRebuilder.ForceRebuildLayoutImmediate(panel.GetComponent<RectTransform>());
     }
 
     public void BackToMateriPanel()
@@ -49,6 +94,7 @@ public class MateriPanel : MonoBehaviour
         contentHiragana.SetActive(false);
         contentKatakana.SetActive(false);
         contentAngka.SetActive(false);
+        contentWaktu.SetActive(false);
         materiPanel.SetActive(true);
     }
 }
